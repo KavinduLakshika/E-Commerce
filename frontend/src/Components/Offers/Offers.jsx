478e09c77aec78c3abf5173278641632eec08fe6
buddhikaeshan <buddhikaeshan12@gmail.com>
2024-10-29 21:32:27 +0530

    navbar

--- frontend/src/Components/Offers/Offers.jsx
+++ frontend/src/Components/Offers/Offers.jsx
@@ -6,14 +6,14 @@ const Offers = () => {
   return (
     <div className="offers-container">
       <div className="offer-card large">
-        <img src='https://img.freepik.com/premium-photo/woman-holding-sign-that-says-black-friday-friday-it_1280538-4401.jpg?w=740' alt='' className="offer-image" />
+        <img src='https://img.freepik.com/free-photo/front-view-young-male-holding-little-packages-sale-writing-yellow-background-gift-color-present-shopping-christmas-holiday-photos_140725-139785.jpg?ga=GA1.1.1937884014.1730188424&semt=ais_hybrid' alt='' className="offer-image" />
       </div>
       <div className="small-images">
         <div className="offer-card small">
-          <img src='https://img.freepik.com/premium-photo/woman-holding-sign-that-says-black-friday-friday-it_1280538-4401.jpg?w=740' alt=''className="offer-image" />
+          <img src='https://img.freepik.com/free-photo/front-view-young-male-holding-little-packages-sale-writing-yellow-background-gift-color-present-shopping-christmas-holiday-photos_140725-139785.jpg?ga=GA1.1.1937884014.1730188424&semt=ais_hybrid' alt=''className="offer-image" />
         </div>
         <div className="offer-card small">
-          <img src='https://img.freepik.com/premium-photo/woman-holding-sign-that-says-black-friday-friday-it_1280538-4401.jpg?w=740' alt='' className="offer-image" />
+          <img src='https://img.freepik.com/free-photo/front-view-young-male-holding-little-packages-sale-writing-yellow-background-gift-color-present-shopping-christmas-holiday-photos_140725-139785.jpg?ga=GA1.1.1937884014.1730188424&semt=ais_hybrid' alt='' className="offer-image" />
         </div>
       </div>
     </div>
