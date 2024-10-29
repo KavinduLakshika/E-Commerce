3fb2163d6c9aabed58aaba4a64ed0f3ab1723f5f
buddhikaeshan <buddhikaeshan12@gmail.com>
2024-10-29 17:55:40 +0530

    about slider

--- frontend/src/Components/About/About.jsx
+++ frontend/src/Components/About/About.jsx
@@ -1,44 +1,30 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './About.css';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
 
 const About = () => {
-  const [currentSlide, setCurrentSlide] = useState(0);
-  const images = [
-    'https://thilakawardhana.com/cdn/shop/files/WEB-COVER_1_f1496fae-56fa-4776-b54a-69e0de417fdb.jpg?v=1727775847&width=1370',
-    'https://thilakawardhana.com/cdn/shop/files/1520-x-592-WEB-5.png?v=1723446577&width=1370',
-    'https://thilakawardhana.com/cdn/shop/files/WEB-COVER_1_f1496fae-56fa-4776-b54a-69e0de417fdb.jpg?v=1727775847&width=1370',
-  ];
-
-  const nextSlide = () => {
-    setCurrentSlide((prevSlide) => (prevSlide + 1) % images.length);
-  };
-
-  const prevSlide = () => {
-    setCurrentSlide((prevSlide) => (prevSlide - 1 + images.length) % images.length);
-  };
-
-  // Auto-slide functionality
-  useEffect(() => {
-    const interval = setInterval(nextSlide, 6000);
-    return () => clearInterval(interval);
-  }, []);
 
   return (
     <div className="slider-container">
-      <div
-        className="slider"
-        style={{ transform: `translateX(-${currentSlide * 100}%)` }}
+      <Carousel
+        showArrows={true}
+        showStatus={false}
+        showThumbs={false}
+        infiniteLoop={true}
+        autoPlay={true}
+        interval={3000}
       >
-        {images.map((image, index) => (
-          <div
-            key={index}
-            className="slide"
-            style={{ backgroundImage: `url(${image})` }}
-          ></div>
-        ))}
-      </div>
-      <button className="prev" onClick={prevSlide}>&#10094;</button>
-      <button className="next" onClick={nextSlide}>&#10095;</button>
+        <div>
+          <img src='https://thilakawardhana.com/cdn/shop/files/WEB-COVER_1_f1496fae-56fa-4776-b54a-69e0de417fdb.jpg?v=1727775847&width=1370' alt="Image 1" />
+        </div>
+        <div>
+          <img src='https://thilakawardhana.com/cdn/shop/files/1520-x-592-WEB-5.png?v=1723446577&width=1370' alt="Image 2" />
+        </div>
+        <div>
+          <img src="https://thilakawardhana.com/cdn/shop/files/1520-x-592-WEB-4.png?v=1723446523&width=1370" alt="Image 3" />
+        </div>
+      </Carousel>
     </div>
   );
 };
