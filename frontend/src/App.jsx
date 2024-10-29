a8ff777bbebd72b009d6277ccd874b856e4b33f8
buddhikaeshan <buddhikaeshan12@gmail.com>
2024-10-29 15:42:01 +0530

    navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import "bootswatch/dist/lux/bootstrap.min.css";
 import Card from "./Components/Card/card";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
     <div>
+      <NavBar/>
       <Card></Card>
     </div>
   )
