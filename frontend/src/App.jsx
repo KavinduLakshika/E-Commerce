efeab107a26bb7e0b0e608d39b35fc0b18464e27
Kavindu Lakshika <109098494+Kavindu-Lakshika@users.noreply.github.com>
2024-10-29 19:47:26 +0530

    App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
-import CardList from "./Components/Card/CardList";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar/NavBar'; 
+import Home from './Pages/Home/Home';
+import CardList from './Components/Card/CardList';
 import prod from './assets/prod.jpg';
 
 function App() {
-
   const products = [
     {
       image: prod,
@@ -16,28 +18,17 @@ function App() {
       prodPrice: 2500,
       availableSizes: ['2XL', 'XL', 'L', 'M', 'S'],
     },
-    {
-      image: prod,
-      prodName: 'Product 2',
-      prodPrice: 2500,
-      availableSizes: ['2XL', 'XL', 'L', 'M', 'S'],
-    },
-    {
-      image: prod,
-      prodName: 'Product 2',
-      prodPrice: 2500,
-      availableSizes: ['2XL', 'XL', 'L', 'M', 'S'],
-    },
   ];
 
   return (
-    <div>
-      <h1>Product List</h1>
-      <CardList
-        data={products}
-      />
-    </div>
-  )
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<CardList data={products} />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
