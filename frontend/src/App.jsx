bbcbd3c8c0ebf3089f420423543f143d23bb2f47
buddhikaeshan <buddhikaeshan12@gmail.com>
2024-10-29 17:24:51 +0530

    app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,17 @@
-import Card from "./Components/Card/card";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar/NavBar';
+import Home from './Pages/Home/Home';
 
 function App() {
   return (
-    <div>
-      <Card></Card>
-    </div>
-  )
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
