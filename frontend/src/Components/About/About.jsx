ef3598cf3b0d31d804d1436ce8d0ed962cfcba34
Maleesha Pathirana <132542186+Maleeshapa@users.noreply.github.com>
2024-10-30 13:22:17 +0530

    img boder

--- frontend/src/Components/About/About.jsx
+++ frontend/src/Components/About/About.jsx
@@ -17,13 +17,13 @@ const About = () => {
         interval={3000}
       >
         <div>
-          <img src='https://thilakawardhana.com/cdn/shop/files/WEB-COVER_1_f1496fae-56fa-4776-b54a-69e0de417fdb.jpg?v=1727775847&width=1370' alt="Image 1" />
+          <img class="rounded" src='https://thilakawardhana.com/cdn/shop/files/WEB-COVER_1_f1496fae-56fa-4776-b54a-69e0de417fdb.jpg?v=1727775847&width=1370' alt="Image 1" />
         </div>
         <div>
-          <img src='https://thilakawardhana.com/cdn/shop/files/1520-x-592-WEB-5.png?v=1723446577&width=1370' alt="Image 2" />
+          <img class="rounded" src='https://thilakawardhana.com/cdn/shop/files/1520-x-592-WEB-5.png?v=1723446577&width=1370' alt="Image 2" />
         </div>
         <div>
-          <img src="https://thilakawardhana.com/cdn/shop/files/1520-x-592-WEB-4.png?v=1723446523&width=1370" alt="Image 3" />
+          <img class="rounded"src="https://thilakawardhana.com/cdn/shop/files/1520-x-592-WEB-4.png?v=1723446523&width=1370" alt="Image 3" />
         </div>
       </Carousel>
     </div>
