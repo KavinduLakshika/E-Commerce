1e14ff057afc3ff1e3f68938f91d8fdc14cc532a
Maleesha Pathirana <132542186+Maleeshapa@users.noreply.github.com>
2024-10-30 14:42:03 +0530

    girls

--- frontend/src/Components/Gallery/Gallery.jsx
+++ frontend/src/Components/Gallery/Gallery.jsx
@@ -7,13 +7,15 @@ function Gallery() {
             <div class="row container">
                 <div class="col-lg-4 col-md-12 mb-4 mb-lg-0 ">
                     <img
-                        src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
+                        // src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
+                        src="https://kandyselection.lk/_next/image?url=https%3A%2F%2Fpub-aab205bc4ae24bca977e05a1c5b36628.r2.dev%2Fu22qckea35oqxf7t7veasvgo&w=1920&q=75"
                         class="w-100 shadow-1-strong rounded mb-4"
                         alt="Boat on Calm Water"
                     />
 
                     <img
-                        src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain1.webp"
+                        //src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain1.webp"
+                        src="https://kandyselection.lk/_next/image?url=https%3A%2F%2Fpub-aab205bc4ae24bca977e05a1c5b36628.r2.dev%2Fhikorvr158kvcefl3exqxzcz&w=3840&q=75"
                         class="w-100 shadow-1-strong rounded mb-4"
                         alt="Wintry Mountain Landscape"
                     />
@@ -21,13 +23,15 @@ function Gallery() {
 
                 <div class="col-lg-4 mb-4 mb-lg-0 ">
                     <img
-                        src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain2.webp"
+                        //src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain2.webp"
+                        src="https://kandyselection.lk/_next/image?url=https%3A%2F%2Fpub-aab205bc4ae24bca977e05a1c5b36628.r2.dev%2For398povxhjk2dov3a3izw7k&w=1920&q=75"
                         class="w-100 shadow-1-strong rounded mb-4"
                         alt="Mountains in the Clouds"
                     />
 
                     <img
-                        src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
+                        //src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
+                        src="https://kandyselection.lk/_next/image?url=https%3A%2F%2Fpub-aab205bc4ae24bca977e05a1c5b36628.r2.dev%2Faut3rz9w4f7se6o7rux6x8u4&w=3840&q=75"
                         class="w-100 shadow-1-strong rounded mb-4"
                         alt="Boat on Calm Water"
                     />
@@ -35,13 +39,15 @@ function Gallery() {
 
                 <div class="col-lg-4 mb-4 mb-lg-0 ">
                     <img
-                        src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(18).webp"
+                        // src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(18).webp"
+                        src="https://kandyselection.lk/_next/image?url=https%3A%2F%2Fpub-aab205bc4ae24bca977e05a1c5b36628.r2.dev%2Fj4fupnw38sdeyrimbndsqnf3&w=828&q=75"
                         class="w-100 shadow-1-strong rounded mb-4"
                         alt="Waves at Sea"
                     />
 
                     <img
-                        src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain3.webp"
+                        //src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain3.webp"
+                        src="https://kandyselection.lk/_next/image?url=https%3A%2F%2Fpub-aab205bc4ae24bca977e05a1c5b36628.r2.dev%2Fi4s6wpa42dfqe6drk0ojdpge&w=1920&q=75"
                         class="w-100 shadow-1-strong rounded mb-4"
                         alt="Yosemite National Park"
                     />
