2b81b10eaedcc9b4506fe2d0aad08047474c3949
Maleesha Pathirana <132542186+Maleeshapa@users.noreply.github.com>
2024-10-30 13:57:31 +0530

    service card

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import About from '../../Components/About/About'
 import Offers from '../../Components/Offers/Offers'
+import Services from '../../Components/Services/Services'
 
 const Home = () => {
   return (
     <div>
       <About />
+      <Services/>
       <Offers/>
     </div>
   )
