6e3c8633728b06e32291bd4499335e052a3ae7eb
buddhikae <buddhikaeshan667@gmail.com>
2024-10-31 00:53:28 +0530

    about

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -4,13 +4,14 @@ import Offers from '../../Components/Offers/Offers'
 import Services from '../../Components/Services/Services'
 import Gallery from '../../Components/Gallery/Gallery'
 import Footer from '../../Components/Footer/Footer'
+import './Home.css'
 
 const Home = () => {
   return (
     <div>
       <About />
       <Services/>
-      <Gallery/>
+      <div className='displayNone'><Gallery/></div>
       <Offers/>
       <Footer/>
     </div>
