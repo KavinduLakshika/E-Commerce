b521506484a42b49bd2d7ad161ee392e3ea7bff3
Kavindu Lakshika <109098494+Kavindu-Lakshika@users.noreply.github.com>
2024-11-01 20:24:01 +0530

    Accessories

--- frontend/src/Pages/Mens/MensWear/InnerWear.jsx
+++ frontend/src/Pages/Mens/MensWear/InnerWear.jsx
@@ -1,37 +1,39 @@
 import CardList from "../../../Components/Card/CardList";
-import prod from '../../../assets/prod.webp';
 import '../../Mens/Mens.css';
+import prod from '../../../assets/prod.webp';
+import prod2 from '../../../assets/prod 2.webp';
+import prod3 from '../../../assets/prod3.webp';
 
 function InnerWear() {
     const sampleData = [
         {
-            prodName: "Product 1",
-            prodPrice: 1000,
-            image: prod,
-            color: "red",
-            availableSizes: ["S", "M", "L", "XL", "2XL", "3XL"]
+            prodName: 'Product 1',
+            prodPrice: 1500,
+            images: [prod, prod2, prod3],
+            color: 'red',
+            availableSizes: ['S', 'M', 'L']
         },
         {
             prodName: "Product 2",
             prodPrice: 2000,
-            image: prod,
+            images: [prod, prod2, prod3],
             color: "blue",
             availableSizes: ['S', 'M', 'L'],
         },
         {
-            prodName: "Product 2",
-            prodPrice: 2500,
-            image: prod,
-            color: "blue",
-            availableSizes: ['S', 'M', 'L'],
+            prodName: 'Product 1',
+            prodPrice: 1500,
+            images: [prod, prod2, prod3],
+            color: 'red',
+            availableSizes: ['S', 'M', 'L']
         },
         {
             prodName: "Product 2",
-            prodPrice: 3000,
-            image: prod,
+            prodPrice: 2000,
+            images: [prod, prod2, prod3],
             color: "blue",
             availableSizes: ['S', 'M', 'L'],
-        },
+        }
     ];
 
     return (
