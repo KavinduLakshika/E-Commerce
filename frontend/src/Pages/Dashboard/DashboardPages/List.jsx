d920e51282655214d56a9a8565da97403c5353fc
buddhikaeshan <buddhikaeshan12@gmail.com>
2024-11-04 01:09:26 +0530

    list navigate

--- frontend/src/Pages/Dashboard/DashboardPages/List.jsx
+++ frontend/src/Pages/Dashboard/DashboardPages/List.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import Sidebar from '../../../Components/SideBar/SideBar'
 import Table from '../../../Components/Table/Table'
+import { useNavigate } from 'react-router-dom'
 
 const List = () => {
-
   const columns = ["ID", "Products"];
   const data = ["ID", "Products"];
   const btnName = 'Add New Product';
 
   const title = 'Product';
   const invoice = 'Product.pdf';
+
+  const navigate=useNavigate();
+
+  const handleAddBtn=()=>{
+    navigate('/add');
+  }
 
   return (
     <div className="body" id="body-pd">
@@ -23,6 +29,7 @@ const List = () => {
           title={title}
           invoice={invoice}
           showDate={false}
+          onAdd={handleAddBtn}
         />
       </div>
 
