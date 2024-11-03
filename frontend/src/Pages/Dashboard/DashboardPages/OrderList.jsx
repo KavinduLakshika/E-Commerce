24b3e777bf34593b9fc79894164948f82b54c19a
buddhikaeshan <buddhikaeshan12@gmail.com>
2024-11-03 21:45:08 +0530

    dash

--- frontend/src/Pages/Dashboard/DashboardPages/OrderList.jsx
+++ frontend/src/Pages/Dashboard/DashboardPages/OrderList.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import Sidebar from '../../../Components/SideBar/SideBar'
+import Table from '../../../Components/Table/Table'
 
 const OrderList = () => {
+  const columns = ["ID", "Products"];
+  const data = ["ID", "Products"];
+  const btnName = 'Add New Product';
+
+  const title = 'Product';
+  const invoice = 'Product.pdf';
+
   return (
     <div className="body" id="body-pd">
       <Sidebar />
       <div>
       <h1>Order List</h1>
+      <Table
+        data={data}
+        columns={columns}
+        btnName={btnName}
+        title={title}
+        invoice={invoice}
+        showDate={false}
+      />
       </div>
     </div>
   )
