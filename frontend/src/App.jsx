46462512ff1b98b3bcf8451839e689ba9978dd53
buddhikae <buddhikaeshan667@gmail.com>
2024-11-03 10:39:25 +0530

    test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
 import Home from './Pages/Home/Home';
 import Mens from './Pages/Mens/Mens';
 import Cart from './Pages/Cart/Cart';
