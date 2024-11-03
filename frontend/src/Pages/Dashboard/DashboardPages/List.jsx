49241a228e6f3a9df30f9f204dd55454dcb54fa1
buddhikaeshan <buddhikaeshan12@gmail.com>
2024-11-03 21:37:14 +0530

    table

--- frontend/src/Pages/Dashboard/DashboardPages/List.jsx
+++ frontend/src/Pages/Dashboard/DashboardPages/List.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
 import Sidebar from '../../../Components/SideBar/SideBar'
+import Table from '../../../Components/Table/Table'
 
 const List = () => {
+
+  const columns = ["ID", "Products"];
+  const data = ["ID", "Products"];
+  const btnName = 'Add New Product';
+
+  const title = 'Product';
+  const invoice = 'Product.pdf';
+
   return (
     <div className="body" id="body-pd">
       <Sidebar />
       <h1>List</h1>
+      <Table
+        data={data}
+        columns={columns}
+        btnName={btnName}
+        title={title}
+        invoice={invoice}
+        showDate={false}
+      />
     </div>
   )
 }
