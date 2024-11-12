da03492927224053a8f3a91506736f0b10017c22
buddhikaeshan <buddhikaeshan12@gmail.com>
2024-11-12 17:18:21 +0530

    list product

--- frontend/src/Pages/Dashboard/DashboardPages/List.jsx
+++ frontend/src/Pages/Dashboard/DashboardPages/List.jsx
@@ -2,18 +2,46 @@ import React from 'react'
 import Sidebar from '../../../Components/SideBar/SideBar'
 import Table from '../../../Components/Table/Table'
 import { useNavigate } from 'react-router-dom'
+import { useEffect, useState } from 'react';
+import config from '../../../config';
 
 const List = () => {
-  const columns = ["ID", "Products"];
-  const data = ["ID", "Products"];
+  const columns = ["ID", "Products Name",'type','Qty','Price','colors'];
+  const [data, setData] = useState([]);
   const btnName = 'Add New Product';
+  const [error, setError] = useState(null);
 
   const title = 'Product';
   const invoice = 'Product.pdf';
 
-  const navigate=useNavigate();
+  useEffect(() => {
+    fetchProductList();
+  },)
 
-  const handleAddBtn=()=>{
+  const fetchProductList = async () => {
+    try {
+      const response = await fetch(`${config.BASE_URL}/products`);
+      if (!response.ok) {
+        setError(`Failed to fetch product: ${response.status} ${response.statusText}`);
+      }
+      const product=await response.json();
+      const formattedData=product.map(product=>[
+        product.productId,
+        product.productName,
+        product.productDescription,
+        product.productQty,
+        product.productPrice,
+        product.productColor
+      ])
+      setData(formattedData);
+    } catch (error) {
+
+    }
+  }
+
+  const navigate = useNavigate();
+
+  const handleAddBtn = () => {
     navigate('/add');
   }
 
